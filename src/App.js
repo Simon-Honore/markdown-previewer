c655e7987133a744406d7a9e0f648c394ea54eb7
Simon-Honore <simon.honore6@gmail.com>
2023-06-28 23:50:37 +0200

    refactor(app):change previewer section into code balise

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ function App() {
         <h1 className='header__title'>markdown previewer</h1>
       </header>
       <main className='main'>
-        <textarea className='area' name="area-markdown" id="area-markdown" cols="30" rows="10"></textarea>
-        <section className='previewer'>
-          <h2 className='previewer__title'>Previewer</h2>
-          <p>previewing markdown</p>
-        </section>
+        <textarea className='area' name="area-markdown" id="area-markdown" cols="30" rows="10">
+				# Prévisualisateur - Believemy
+				### Oh ! De la magie !
+				En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !
+				### Cas d'utilisation
+				- *italique* 
+				- **gras**
+				- `monospace`
+				- ~~barré~~
+				- # h1
+				- ## h2
+				- ### h3
+				- #### etc
+				[Believemy](https://believemy.com)
+        </textarea>
+        <code className='previewer'></code>
       </main>
 
     </div>
