165412ca8983281a21af367955ff4068e9c99a4f
Simon-Honore <simon.honore6@gmail.com>
2023-06-28 23:32:31 +0200

    (feat(app):static page with css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>markdown previewer</h1>
+      <header className='header'>
+        <h1 className='header__title'>markdown previewer</h1>
+      </header>
+      <main className='main'>
+        <textarea className='area' name="area-markdown" id="area-markdown" cols="30" rows="10"></textarea>
+        <section className='previewer'>
+          <h2 className='previewer__title'>Previewer</h2>
+          <p>previewing markdown</p>
+        </section>
+      </main>
+
     </div>
   );
 }
